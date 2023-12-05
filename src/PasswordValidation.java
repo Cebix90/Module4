f632edcc78528ff37871d1133005af4b22042c74
Cebix90 <cebulmat@gmail.com>
2023-12-05 23:34:40 +0100

    hasNoRepetitions method implemented

--- src/PasswordValidation.java
+++ src/PasswordValidation.java
@@ -6,4 +6,14 @@ public class PasswordValidation {
     public boolean hasNoWhiteSpaces(String password){
         return !password.matches(".*\\s.*");
     }
+
+    public boolean hasNoRepetitions(String password){
+        for(int i = 0; i < password.length() - 1; i++){
+            if(password.charAt(i) == password.charAt(i + 1)){
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
