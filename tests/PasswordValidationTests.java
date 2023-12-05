d1a3f0c3cfc4ef650ed41f8ba4c622426fd808e0
Cebix90 <cebulmat@gmail.com>
2023-12-05 23:07:21 +0100

    testHasNoWhiteSpaces method implemented

--- tests/PasswordValidationTests.java
+++ tests/PasswordValidationTests.java
@@ -2,23 +2,34 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class PasswordValidationTests {
+
+    private final PasswordValidation passwordValidation;
+
+    public PasswordValidationTests() {
+        passwordValidation = new PasswordValidation();
+    }
+
     @Test
     public void testHasGoodLength(){
-        PasswordValidation passwordValidation = new PasswordValidation();
-
-        String password = "Topsecretkey123";
-
+        String password = "Topsecretkey123!";
         boolean lengthResult = passwordValidation.hasGoodLength(password);
+
         Assertions.assertTrue(lengthResult);
     }
 
     @Test
     public void testHasWrongLength(){
-        PasswordValidation passwordValidation = new PasswordValidation();
-
         String password = "Toshortsad1";
-
         boolean lengthResult = passwordValidation.hasGoodLength(password);
+
         Assertions.assertFalse(lengthResult);
+    }
+
+    @Test
+    public void testHasNoWhiteSpaces(){
+        String password = "Topsecretkey123!";
+        boolean noWhiteSpacesResult = passwordValidation.hasNoWhiteSpaces(password);
+
+        Assertions.assertTrue(noWhiteSpacesResult);
     }
 }
