7648f8747555048c8e604650cc100cc34c90b779
Cebix90 <cebulmat@gmail.com>
2023-12-05 23:20:06 +0100

    hasNoWhiteSpaces method implemented

--- src/PasswordValidation.java
+++ src/PasswordValidation.java
@@ -2,4 +2,8 @@ public class PasswordValidation {
     public boolean hasGoodLength(String password){
         return password.length() >= 12;
     }
+
+    public boolean hasNoWhiteSpaces(String password){
+        return !password.matches(".*\\s.*");
+    }
 }
